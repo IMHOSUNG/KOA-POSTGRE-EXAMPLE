c004bcc91e891d57b3d21887bc6df21eac7b7293
IMHOSUNG <dlaghtjd4487@naver.com>
2019-09-27 13:22:52 +0900

    websocket example

--- src/routes/ws-routes.ts
+++ src/routes/ws-routes.ts
@@ -4,5 +4,4 @@ import controller = require('controllers');
 export const wsRouter = new Router();
 
 //Routes for the user entity
-wsRouter.get('/ws', controller.wsaction.accessTest);          //Create some test users
-wsRouter.get('/ws/test', controller.wsaction.accessTest);
+wsRouter.all('/ws', controller.wsaction.accessTest);
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,16 @@ import {restRouter} from 'routes/rest-routes';
 import {wsRouter} from 'routes/ws-routes';
 import * as bodyParser from 'koa-bodyparser';
 import Route = require('koa-router')
-var router = new Route<any>();
 var app = require('./app')
 import WebSocket = require('ws');
-import * as Koa from 'koa';
-/*
+import http = require('http');
+const server = http.createServer(app)
+
+const serverTime = () => {
+}
+
 const bootstrap = async () => {
+
 
     // Initialize the database
     await postgresDB();
@@ -25,27 +29,44 @@ const bootstrap = async () => {
     //Tell our application to use the router we have created to handle routes for our rest api
     app.use(restRouter.routes(), restRouter.allowedMethods())
 
-    app.ws.use(wsRouter.routes(), wsRouter.allowedMethods())
-    console.log(app.ws.clients)
     //Tell the app to listen on port 3000
     app.listen(3000);
-};*/
-import http = require('http');
-const server = http.createServer(app)
+};
+
 
 const wsConnect = async () => {
-    const wss = new WebSocket.Server({ server });
- 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    ws.send('something');
-  });
- 
-  ws.send('something');
+    const wss = new WebSocket.Server({server});
+    app.use(bodyParser());
+    app.use(wsRouter.routes(), wsRouter.allowedMethods())
+
+    wss.on('connection', (ws:WebSocket)=> {
+      ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+
+        const broadcastRegex = /^broadcast\:/;
+
+        if (broadcastRegex.test(message)) {
+            message = message.replace(broadcastRegex, '');
+
+            //send back the message to the other clients
+            wss.clients
+                .forEach(client => {
+                    //if (client != ws) {
+                        client.send(`Hello, broadcast message -> ${message}`);
+                    //}    
+                });
+            
+        } else {
+            ws.send(`Hello, you sent -> ${message}`);
+        }
+      });
+      ws.send('Hi there, I am a WebSocket Server')
     });
-    server.listen(3000,()=>{
-        console.log("3000 start")
+    
+    app.listen(4000,()=>{
+        console.log(`${4000} start`)
     });
 }
 
