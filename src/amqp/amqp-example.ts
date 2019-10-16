68e6b068ece7fe95bf55ed70c74f50b4199e7530
IMHOSUNG <dlaghtjd4487@naver.com>
2019-10-16 19:12:08 +0900

    STUDY : AMQP RabbitMQ routing key, topic, exchange

--- src/amqp/amqp-example.ts
+++ src/amqp/amqp-example.ts
@@ -4,6 +4,15 @@
 import * as amqp from 'amqplib'
 import * as MQConfig from './amqp-config'
 
+const randString = () => {
+    let str = Math.random()
+            .toString(36)
+            .replace(/[^a-z]+/g,'')
+            .substr(0,5);
+
+    return str;
+}
+
 export const MQConnection = async() => {
     
     let config = await MQConfig.ConnectInfo();
@@ -40,8 +49,10 @@ const send = async() => {
 
     let queue = 'hello';
     let msg = "hello world"
-    await ch.assertQueue(queue,{ durable : false})
-    ch.sendToQueue(queue, Buffer.from(msg));
+    //message queue를 생성할 때 durable : true로 설정할 경우 RabbitMq가 죽어도 
+    // task를 잃어 버리지 않는다.
+    await ch.assertQueue(queue,{ durable : true})
+    ch.sendToQueue(queue, Buffer.from(msg),{persistent : true});
     console.log("[x] 보낸 %s", msg);
 
     setTimeout(() => {
@@ -56,11 +67,25 @@ const receive = async() => {
     
     let queue = 'hello'
 
-    ch.assertQueue(queue,{ durable : false})
+    //이미 만들어진 큐는 적용이 안된다. >> 다른 큐를 만들어서 적용한다. 
+    // durable을 true로 하면 큐를 다시 재시작해도 메시지를 잃어버리지 않는다.
+    ch.assertQueue(queue,{ durable : true})
+
+    // 1개씩 만 받아오는 명령어
+    ch.prefetch(1);
     console.log("[*] Waiting for message in %s. To exit process CTRL+C",queue);
 
     ch.consume(queue, (msg:amqp.ConsumeMessage)=>{
         console.log("[x] Received %s",msg.content.toString());
+        setTimeout(()=>{
+            console.log("log [x] Done");
+            ch.ack(msg);
+        }, 10);
+    },
+    // worker을 ctrl+c로 죽여도, 메시지가 없어지지 않는다. 
+    // 워커가 일을 하고 워커로 부터 적절한 ack 시그널을 받는다. 
+    {
+        noAck : false
     })
 }
 
@@ -81,7 +106,123 @@ const newTask = async() => {
         process.exit(0);
     },500);
 }
-//send();
-//send();
-receive();
 
+
+const exchangeEmitLog = async() => {
+    let con = await MQConnection();
+    let ch = await con.createChannel();
+    
+    let exchangeMachine = 'log';
+    let msg = process.argv.slice(2).join('') || randString();
+
+    ch.assertExchange( exchangeMachine , 'fanout', {
+        durable : false
+    })
+
+    ch.publish(exchangeMachine, '' , Buffer.from(msg));
+
+    console.log("[x] 보낸 %s", msg);
+}
+
+const receive_Log_From_Exchange_Machine = async() => {
+
+    let con = await MQConnection();
+    let ch = await con.createChannel();
+
+    let exchangeMachine = 'log';
+    
+    ch.assertExchange(exchangeMachine, 'fanout', {
+        durable : false
+    })
+
+    let q = await ch.assertQueue(``, {
+        exclusive : true
+    })
+    
+    console.log("[*] %s 의 메시지 대기 중, 종료하려면 Ctrl + C를 누르십시오", q.queue);
+    // exchange Maching 과 queue를 연결 
+    ch.bindQueue(q.queue, exchangeMachine, '');
+    ch.consume(q.queue, (msg:amqp.ConsumeMessage)=>{
+        if(msg.content){
+            console.log(
+                "[x] %s", msg.content.toString()
+            )
+        }
+    })
+    ,{
+        noAck : true       
+    }
+    
+}
+
+const DirectEmitLog = async() => {
+
+    //command 
+    // command error "Run. Run. Or it will explode"
+
+    let con = await MQConnection();
+    let ch = await con.createChannel();
+
+    let exchangeMN = 'direct_logs';
+    let msg = randString();
+    
+    let args = process.argv.slice(2);
+    console.log(args);
+    let serverity = (args.length > 0) ? args[0]: 'info';
+
+    //topic 방식
+    //exchange를 topic으로 바꾸면 된다.
+    ch.assertExchange(exchangeMN, 'direct', {
+        durable : false
+    })
+
+    ch.publish(exchangeMN, serverity , Buffer.from(msg));
+    console.log("[x] Sent %s: '%s'", serverity, msg);
+}
+
+
+const receiveLogDirect = async() => {
+    let con = await MQConnection();
+    let ch = await con.createChannel();
+
+    let args = process.argv.slice(2);
+    console.log(args);
+
+    if (args.length == 0) {
+    console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+    process.exit(1);
+    }
+
+    let exchangeMN = 'direct_logs';
+    
+    //topic 방식
+    //exchange를 topic으로 바꾸면 된다.
+    ch.assertExchange(exchangeMN, 'direct',{
+        durable : false
+    })
+
+    let q = await ch.assertQueue('')
+    console.log('[*] Waiting for logs. To exit press CTRL+C');
+    args.forEach((severity)=>{
+        ch.bindQueue(q.queue, exchangeMN, severity);
+    })
+    ch.consume(q.queue, (msg)=>{
+        console.log(" [x] %s %s", msg.fields.routingKey,msg.content.toString());
+
+    },{
+        noAck : true
+    })
+}
+
+// routerkey와 topic 차이
+// Routerkey = 'direct'의 Routing Key
+// topic = 'topic'
+// 점(.) 으로 구분된 단어 목록 topic은 임의의 routing_key를 가질 수 없다.
+// * > 정확히 한 단어를 대체 할 수 있다.
+// # > 0개 이상의 단어를 대체할 수 있다.
+
+//send();
+//send();
+//receive();
+//receive_Log_From_Exchange_Machine();
+receiveLogDirect();
